85613d4dd1677753a3eb1145a936fe5d688bdb0d
edaakyil <edaakyil@gmail.com>
2025-01-03 14:13:24 +0300

    update

--- src/main/kotlin/com/edaakyil/kotlin/math/Complex.kt
+++ src/main/kotlin/com/edaakyil/kotlin/math/Complex.kt
@@ -1,14 +1,17 @@
 package com.edaakyil.kotlin.math
 
+import kotlin.math.absoluteValue
 import kotlin.math.sqrt
 
-class Complex(val real: Double = 0.0, val imaginary: Double = 0.0) {
+data class Complex(val real: Double = 0.0, val imaginary: Double = 0.0) {
     val norm: Double
         get() = sqrt(real * real + imaginary * imaginary)
-
     val length: Double
         get() = norm
-
     val conjugate: Complex
         get() = Complex(real, -imaginary)
+
+    override fun equals(other: Any?) = other is Complex && (real - other.real).absoluteValue < 0.00001 && (imaginary - other.imaginary).absoluteValue < 0.00001
+
+    override fun toString() = "($real, $imaginary)"
 }
